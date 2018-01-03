74f3e0207902575c219d41def835718212f39f08
Sumit Bhagirath <sumitbhagirath@gmail.com>
2018-01-03 23:08:52 +0530

    rooms

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //https://www.youtube.com/watch?v=dOSIqJWQkXM&list=PLicY6aYZ8ilpmHfJ8jP1lt7ihPpRWBJ9P&index=2
+//https://divillysausages.com/2015/07/12/an-intro-to-socket-io/
 const express = require('express'),
   app = express(),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server),
-  users = [];
-  connected = [];
+  users = [],
+  connected = [],
+  rooms = ['room1','room2','room3'];
+  ;
 
 
 server.listen(7002, () => {
@@ -27,6 +30,12 @@ io.sockets.on('connection', (socket)=>{
       connected[socket.nickname]=socket
       users.push({username:socket.nickname,socketId:socket.id})
       updateNickNames()
+
+      // rooms
+      socket.room = 'room1'
+      socket.join('room1')
+      socket.broadcast.to('room1').emit('updatechat', 'SERVER', socket.nickname + ' has connected to this room');
+      socket.emit('updaterooms', rooms, 'room1');
     }
   })
   function updateNickNames(){
@@ -49,13 +58,36 @@ io.sockets.on('connection', (socket)=>{
   socket.on('attachment', function (msg) {
       //console.log(msg);
       io.sockets.emit('attachmentMsg', socket.nickname, msg);
-  });
+  })
   socket.on('privateAttachment', function (data) {
       console.log(data.message);
       io.sockets.connected[socket.id].emit('ownPrivateAttachmentMsg', {msg:data.message, by: {username:socket.nickname,socketId:socket.id}, to: {username:data.username,socketId:data.socketId}});
       socket.broadcast.to(data.socketId).emit('privateAttachmentMsg',{msg:data.message, by: {username:socket.nickname,socketId:socket.id}, to: {username:data.username, socketId:data.socketId}})
       //io.sockets.emit('privateAttachmentMsg', socket.nickname, msg);
-  });
+  })
+
+  // when the client emits 'sendchat', this listens and executes
+  socket.on('sendchat', function (data) {
+    // we tell the client to execute 'updatechat' with 2 parameters
+    io.sockets.in(socket.room).emit('updatechat', socket.nickname, data);
+  })
+
+  socket.on('switchRoom', function(newroom){
+    socket.leave(socket.room);
+    socket.join(newroom);
+    //socket.emit('updatechat', 'SERVER', 'you have connected to '+ newroom);
+    // sent message to OLD room
+    socket.broadcast.to(socket.room).emit('updatechat', 'SERVER', socket.nickname+' has left this room');
+    // update socket session room title
+    socket.room = newroom;
+    socket.broadcast.to(newroom).emit('updatechat', 'SERVER', socket.nickname+' has joined this room');
+    socket.emit('updaterooms', rooms, newroom);
+  })
+
+  socket.on('room send message', (data) => {  
+    io.sockets.connected[socket.id].emit('own new room message', {msg:data.message, by:socket.nickname});  
+    socket.broadcast.to(socket.room).emit('new room message', {msg:data.message, by:socket.nickname})
+  })
 
   socket.on('disconnect', (data)=>{
     if(!socket.nickname) return;    
@@ -64,5 +96,7 @@ io.sockets.on('connection', (socket)=>{
     });
     delete connected[socket.nickname]
     updateNickNames()
+    socket.broadcast.emit('updatechat', 'SERVER', socket.nickname + ' has disconnected')
+    socket.leave(socket.room)
   })
 })
